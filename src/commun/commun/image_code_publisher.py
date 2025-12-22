fbc8dc3b43d0a189eb515ed5b041d227c3364a1e
Leonard Ngo <ngoak@islab.snu.ac.kr>
2025-12-22 12:03:26 +0900

    vitgan encoder jetson implementation

--- src/commun/commun/image_code_publisher.py
+++ src/commun/commun/image_code_publisher.py
@@ -4,6 +4,8 @@ from rclpy.node import Node
 import numpy as np
 import cv2
 
+from cv_bridge import CvBridge
+
 from skimage.transform import resize
 
 import torch
@@ -24,9 +26,12 @@ class ImageCodePublisher(Node):
 
 	def __init__(self):
 		super().__init__('image_code_publisher')
+
+		self.bridge = CvBridge()
 		self.subscription = self.create_subscription(
-			CompressedImage,
-			'/camera/camera/color/image_raw/compressed',
+			# CompressedImage,
+			Image,
+			'/camera/camera/color/image_raw',
 			self.listener_callback,
 			1
 		)
@@ -35,34 +40,21 @@ class ImageCodePublisher(Node):
 			1
 		)
 
-		config = get_config_from_file("/home/leonard/semcom_ws/src/commun/commun/imagenet_vitvq_small.yaml")
-		vitvq: ViTVQ = initialize_from_config(config.model)
-		vitvq.init_from_ckpt("/home/leonard/semcom_ws/src/commun/commun/checkpoint/imagenet_vitvq_small.ckpt")
-		for param in vitvq.parameters():
+		config = get_config_from_file("/home/nvidia/semcom_ws/src/commun/commun/imagenet_vitvq_encoder_base.yaml")
+
+		self.vitvq: ViTVQ = initialize_from_config(config.model)
+		for param in self.vitvq.parameters():
 			param.requires_grad = False
-		vitvq.eval()
-		vitvq.cuda()
-		
-		self.encoder = vitvq.encoder
-		self.quantizer = vitvq.quantizer
-		self.pre_quant = vitvq.pre_quant
-		del vitvq
+		self.vitvq.eval()
+		self.vitvq.cuda()
 
 		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 		self.l = []
 
 		self.bits_per_code = 13
-
-	def encode_codes(self, x: torch.FloatTensor) -> torch.LongTensor:
-		h = self.encoder(x)
-		h = self.pre_quant(h)
-		_, _, codes = self.quantizer(h)
 		
-		return codes
-	
-	def listener_callback(self, msg: CompressedImage):
+	def listener_callback(self, msg: Image):
 		now_ns = self.get_clock().now().nanoseconds
-		# self.get_logger().info('I heard: "%s"' % msg.header.stamp)
 
 		ts_ns = int(msg.header.stamp.sec) * 1_000_000_000 + int(msg.header.stamp.nanosec)
 		latency_ns = now_ns - ts_ns
@@ -72,47 +64,52 @@ class ImageCodePublisher(Node):
 		if len(self.l) > 100: self.l.pop(0)
 
 		self.get_logger().info(f'Latency: {latency_ms:4f} ms ({np.mean(self.l):.6f} ms)')
-		# self.get_logger().info(f'Latency: {latency_ms:4f} ms')
 
-
-		np_arr = np.frombuffer(msg.data, dtype=np.uint8)
-		image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # BGR
-
-		image_resized = resize(image, (256, 256), anti_aliasing=True)
+		img = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+		
+		image_resized = resize(
+			img, (256, 256),
+			preserve_range=True, anti_aliasing=True
+		).astype(np.uint8)
 		input_data = (
 			torch.from_numpy(image_resized)
 			.permute(2, 0, 1)
 			.unsqueeze(0)
-			.to(self.device, non_blocking=True)
+			.to(self.device)
 			.float()
 			/ 255.0
 		)
-		# with torch.no_grad():
+		
 		# encoding
-		with torch.no_grad(): #, torch.autocast("cuda", torch.float16):
-			h = self.encoder(input_data)
-			h = self.pre_quant(h)
-			_, _, codes = self.quantizer(h)
-		self.get_logger().info(f'codes: {codes.shape}')
+		with torch.no_grad(), torch.autocast("cuda", torch.float16):
+			codes = self.vitvq.encode_codes(input_data)
 
-		codes = codes.detach().to("cpu").contiguous()
-		codes_np = codes.view(-1).numpy().astype(np.uint16)
+		codes = codes.detach().to("cpu").contiguous().numpy()
+		self.get_logger().info(f'codes: {"-".join([str(c) for c in codes[0, :10]])}')
 
-		# pack bits into bytes
-		packed = np.packbits(
-			np.unpackbits(codes_np[:, None].view(np.uint8), axis=1)[:, -self.bits_per_code:],
-			axis=1
-		).reshape(-1)
+		codes = codes.astype(np.uint16).reshape(-1)
+		bits = ((codes[:, None] >> np.arange(12, -1, -1)) & 1).astype(np.uint8)
 
-		msg = Code()
-		msg.header = Header()
-		msg.header.stamp = self.get_clock().now().to_msg()
-		msg.header.frame_id = "codes"
+		# flatten bitstream
+		bitstream = bits.reshape(-1)
 
-		msg.length = 1024
-		msg.data = packed.tolist()  # uint8[]
+		# pad to byte boundary
+		pad = (-len(bitstream)) % 8
+		if pad:
+			bitstream = np.pad(bitstream, (0, pad), constant_values=0)
 
-		self.publisher.publish(msg)
+		# bits → bytes
+		packed = np.packbits(bitstream, bitorder="big")
+
+		_msg = Code()
+		_msg.header = Header()
+		_msg.header.stamp = self.get_clock().now().to_msg()
+		_msg.header.frame_id = msg.header.frame_id
+
+		_msg.length = 1024
+		_msg.data = packed.tolist()  # uint8[]
+
+		self.publisher.publish(_msg)
 
 
 def main(args=None):
@@ -131,3 +128,13 @@ def main(args=None):
 
 if __name__ == '__main__':
 	main()
+
+
+
+		# config = get_config_from_file("/home/nvidia/semcom_ws/src/commun/commun/imagenet_vitvq_encoder_base.yaml")
+
+		# self.vitvq_base: ViTVQ = initialize_from_config(config.model)
+		# for param in self.vitvq.parameters():
+		# 	param.requires_grad = False
+		# self.vitvq_base.eval()
+		# self.vitvq_base.cuda()
--- src/commun/commun/image_code_subscriber.py
+++ src/commun/commun/image_code_subscriber.py
@@ -4,43 +4,133 @@ from rclpy.node import Node
 import numpy as np
 import cv2
 
+from skimage.transform import resize
+
+import torch
+from torch import nn
+
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, CompressedImage
 # from .vitvq-gan import ViTVQ
-# from enhancing import ViTVQ
-# from enhancing import ViTEncoder as Encoder, ViTDecoder as Decoder
-# from enhancing import VectorQuantizer, GumbelQuantizer
+from enhancing.modules.stage1.vitvqgan import ViTVQ
+from enhancing.modules.stage1.layers import ViTEncoder as Encoder, ViTDecoder as Decoder
+from enhancing.modules.stage1.quantizers import VectorQuantizer, GumbelQuantizer
+from enhancing.utils.general import get_config_from_file, initialize_from_config
+
+from std_msgs.msg import Header
+from semcom_msgs.msg import Code
 
 
 class ImageCodeSubscriber(Node):
 
-    def __init__(self):
-        super().__init__('image_raw_subscriber')
-        self.subscription = self.create_subscription(
-            CompressedImage,
-            '/camera/camera/color/image_raw/compressed',
-            self.listener_callback,
-            1
-        )
-        self.subscription  # prevent unused variable warning
-        self.l = []
+	def __init__(self):
+		super().__init__('image_code_subscriber')
+		self.subscription = self.create_subscription(
+			Code,
+			'/camera/camera/color/image_raw/code',
+			self.listener_callback,
+			10
+		)
+		self.publisher = self.create_publisher(
+			Image,
+			'/decode',
+			1
+		)
 
-    def listener_callback(self, msg: CompressedImage):
-        pass
+		config = get_config_from_file("./imagenet_vitvq_base.yaml")
+		vitvq: ViTVQ = initialize_from_config(config.model)
+		vitvq.init_from_ckpt("./checkpoint/imagenet_vitvq_base.ckpt")
+		for param in vitvq.parameters():
+			param.requires_grad = False
+		vitvq.eval()
+		vitvq.cuda()
+		
+		self.quantizer = vitvq.quantizer
+		self.post_quant = vitvq.post_quant
+		self.decoder = vitvq.decoder
+		# del vitvq
 
+		self.bits_per_code = 13
+		self.l = []
+
+		self.bridge = CvBridge()
+
+	def encode_codes(self, x: torch.FloatTensor) -> torch.LongTensor:
+		h = self.encoder(x)
+		h = self.pre_quant(h)
+		_, _, codes = self.quantizer(h)
+		
+		return codes
+	
+	def listener_callback(self, msg: Code):
+		now_ns = self.get_clock().now().nanoseconds
+		# self.get_logger().info('I heard: "%s"' % msg.header.stamp)
+
+		ts_ns = int(msg.header.stamp.sec) * 1_000_000_000 + int(msg.header.stamp.nanosec)
+		latency_ns = now_ns - ts_ns
+		latency_ms = latency_ns / 1e6
+
+		self.l.append(latency_ms)
+		if len(self.l) > 100: self.l.pop(0)
+
+		self.get_logger().info(f'Latency: {latency_ms:4f} ms ({np.mean(self.l):.6f} ms)')
+		
+		# parsing
+		byte_arr = np.frombuffer(msg.data, dtype=np.uint8)
+		bitstream = np.unpackbits(byte_arr, bitorder="big")
+		bitstream = bitstream[:msg.length*self.bits_per_code]
+		bits = bitstream.reshape(msg.length, self.bits_per_code)
+		codes_np = (bits << np.arange(12, -1, -1)).sum(axis=1).astype(np.uint16)
+
+		if codes_np.size != msg.length:
+			self.get_logger().error(f"Code length mismatch: {codes_np.shape}-{msg.length}")
+			return
+
+		self.get_logger().info(f'codes: {"-".join([str(c) for c in codes_np[:10]])}')
+		codes = torch.from_numpy(codes_np).long().unsqueeze(0).cuda()
+
+		# decoding
+		with torch.no_grad():
+			quant = self.quantizer.embedding(codes.cuda())
+			quant = self.quantizer.norm(quant)
+			
+			if self.quantizer.use_residual:
+				quant = quant.sum(-2)  
+				
+			quant = self.post_quant(quant)
+			dec = self.decoder(quant)
+
+		dec_img = dec.squeeze(0).permute(1, 2, 0).cpu().numpy()
+		dec_img = dec_img.clip(0, 1)
+
+		dec_img = (dec_img * 255).astype(np.uint8)
+		# plt.imshow(dec_img)
+		# plt.savefig("./img.png")
+		# resize: (width=424, height=240)
+		dec_img = cv2.resize(
+			dec_img, (424, 240),
+			interpolation=cv2.INTER_LINEAR
+		)
+		_msg = self.bridge.cv2_to_imgmsg(dec_img, encoding="rgb8")
+		_msg.header.stamp = self.get_clock().now().to_msg()
+		_msg.header.frame_id = msg.header.frame_id
+
+		self.publisher.publish(_msg)
+		
 
 def main(args=None):
-    rclpy.init(args=args)
+	rclpy.init(args=args)
 
-    minimal_subscriber = ImageCodeSubscriber()
+	subscriber = ImageCodeSubscriber()
 
-    rclpy.spin(minimal_subscriber)
+	rclpy.spin(subscriber)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
-    rclpy.shutdown()
+	# Destroy the node explicitly
+	# (optional - otherwise it will be done automatically
+	# when the garbage collector destroys the node object)
+	subscriber.destroy_node()
+	rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    main()
+	main()
